66dc7af03cf42f1212761ed356f713052f7330f8
Rich Hildebrand <richhildebrand@gmail.com>
2013-07-15 22:56:58 -0400

    Init - We count seconds, always and forever.

---
+++ PhoneApp1/Models/StopWatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerUI.Models
+{
+    public class StopWatch
+    {
+        public int Seconds { get; set; } 
+    }
+}
---
+++ PhoneApp1/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Threading;
+using TimerUI.Models;
+namespace TimerUI.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private DispatcherTimer _dispatcherTimer;
+        private StopWatch _stopWatch;
+        private int _seconds;
+
+        public int Seconds
+        {
+            get
+            {
+                return _seconds;
+            }
+            set
+            {
+                if (_seconds != value)
+                {
+                    _seconds = value;
+                }
+
+                OnPropertyChanged("Seconds");
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            _stopWatch = new StopWatch();
+            _stopWatch.Seconds = 0;
+
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += _dispatcherTimer_Tick;
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            _dispatcherTimer.Start();
+        }
+
+
+
+        private void _dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            _stopWatch.Seconds += 1;
+            Seconds = _stopWatch.Seconds;
+        }
+
+    }
+}

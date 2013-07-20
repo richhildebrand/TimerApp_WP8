9caf52d30156ef1f2c0f0097f20da0274502bf74
Eric Fleming <eric-fleming18@hotmail.com>
2013-07-19 23:48:38 -0400

    Rich, see comment - Instructions on running unit tests

--- Unit Tests/ButtonClicked/StartButtonShould.cs
+++ Unit Tests/ButtonClicked/StartButtonShould.cs
@@ -11,6 +11,15 @@ namespace Unit_Tests.ButtonClicked
         [TestMethod]
         public void ChangeButtonTextToStop()
         {
+            //This test will probably change because the logic will probably change but I just wanted
+            //to get a unit test in here. Below I'll put a few instructions on how to run these.
+            //Basicaly, once you have it pulled down from GitHub, set it as the startup project.
+            //Once you do that, you should be able to run it in the emulator and push the little
+            //arrow at the bottom and that will run the tests. Funny thing is I found an article written
+            //by a Teleriker that I met at Stir Trek. The article is here:
+            //http://michaelcrump.net/setting-up-unit-testing-in-windows-phone-7-and-8
+            //I'm really excited about this
+
             var mainPageViewModel = new MainPageViewModel();
             var thing = new Object();
             mainPageViewModel.StartCounter(thing);

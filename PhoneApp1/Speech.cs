8645ed017bae2475d4976a52f1d47e1aeb07da0d
Rich Hildebrand <richhildebrand@gmail.com>
2013-07-21 14:42:32 -0400

    First iteration of voice commands

---
+++ PhoneApp1/Speech.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Phone.Speech.Recognition;
+using Windows.Phone.Speech.Synthesis;
+
+namespace TimerUI
+{
+    public static class Speech
+    {
+        public static SpeechRecognizer recognizer;
+        public static SpeechSynthesizer synthesizer;
+        public static SpeechRecognizerUI recognizerUI;
+
+        private static bool initialized = false;
+
+        // Must be called before using static methods.
+        public static void Initialize()
+        {
+            if (Speech.initialized)
+                return;
+
+            Speech.recognizer = new SpeechRecognizer();
+            Speech.synthesizer = new SpeechSynthesizer();
+            Speech.recognizerUI = new SpeechRecognizerUI();
+
+            // Sets the en-US male voice.
+            IEnumerable<VoiceInformation> enUSMaleVoices = from voice in InstalledVoices.All
+                                                           where voice.Gender == VoiceGender.Male
+                                                           && voice.Language == "en-US"
+                                                           select voice;
+
+            Speech.synthesizer.SetVoice(enUSMaleVoices.ElementAt(0));
+
+            Speech.initialized = true;
+        }
+    }
+}
--- PhoneApp1/ViewModel/MainPageViewModel.cs
+++ PhoneApp1/ViewModel/MainPageViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Messaging;
 using TimerUI.Interfaces;
 using TimerUI.ViewModels.Commands;
 using GalaSoft.MvvmLight;
+using Windows.Phone.Speech.Recognition;
+using Microsoft.Phone.Controls;
 
 namespace TimerUI.ViewModel
 {
@@ -19,10 +22,36 @@ namespace TimerUI.ViewModel
             _stopWatch = stopWatch;
             Seconds = "0";
             Messenger.Default.Register<StopWatch>(this, OnStopWatchTick);
+            Speech.Initialize();
         }
 
         public MainPageViewModel() : this(new StopWatch())
         {
+        }
+
+        private async void ListenForStartCommand(object sender)
+        {
+            SpeechRecognitionUIResult result;
+            result = await Speech.recognizerUI.RecognizeWithUIAsync();
+            if (result.RecognitionResult.Text.Contains("Start"))
+            {
+                _stopWatch.Start();
+                await Speech.synthesizer.SpeakTextAsync("Timer Started");
+                ListenForStopCommand(sender);
+            }
+            else { ListenForStartCommand(sender); }
+        }
+
+        private async void ListenForStopCommand(object sender)
+        {
+            SpeechRecognitionUIResult result;
+            result = await Speech.recognizerUI.RecognizeWithUIAsync();
+            if (result.RecognitionResult.Text.Contains("Stop"))
+            {
+                _stopWatch.Stop();
+                await Speech.synthesizer.SpeakTextAsync("Timer stopped at " + Seconds + " seconds.");
+            }
+            else { ListenForStopCommand(sender); }
         }
 
         private void OnStopWatchTick(StopWatch stopWatch)
@@ -45,6 +74,11 @@ namespace TimerUI.ViewModel
         public ICommand StartButtonClick
         {
             get { return new DelegateCommand(ToggleStartAndStopButton, CanExecute); }
+        }
+
+        public ICommand ActivateVoiceCommandsClick
+        {
+            get { return new DelegateCommand(ListenForStartCommand, CanExecute); }
         }
 
         public bool CanExecute(object returnsTrue)

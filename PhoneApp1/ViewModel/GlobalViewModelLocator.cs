f1021c9513b6c16a02604be000c2764446fe4dd1
Rich Hildebrand <richhildebrand@gmail.com>
2013-07-23 11:09:51 -0400

    Timeout settings displays voice timeout

---
+++ PhoneApp1/ViewModel/GlobalViewModelLocator.cs
@@ -0,0 +1,145 @@
+﻿/*
+  In App.xaml:
+  <Application.Resources>
+      <vm:GlobalViewModelLocator xmlns:vm="clr-namespace:WindowsPhoneApplication6.ViewModels"
+                                   x:Key="Locator" />
+  </Application.Resources>
+  
+  In the View:
+  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+*/
+
+namespace TimerUI.ViewModel
+{
+    public class GlobalViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the GlobalViewModelLocator class.
+        /// </summary>
+        public GlobalViewModelLocator()
+        {
+            CreateMainPageViewModel();
+            CreateTimeoutSettingsViewModel();
+        }
+
+        #region MainPageViewModel
+        private static MainPageViewModel _mainPageViewModel;
+
+        /// <summary>
+        /// Gets the MainPageViewModel property.
+        /// </summary>
+        public static MainPageViewModel MainPageViewModelStatic
+        {
+            get
+            {
+                if (_mainPageViewModel == null)
+                {
+                    CreateMainPageViewModel();
+                }
+
+                return _mainPageViewModel;
+            }
+        }
+
+        /// <summary>
+        /// Gets the MainPageViewModel property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public MainPageViewModel MainPageViewModel
+        {
+            get
+            {
+                return MainPageViewModelStatic;
+            }
+        }
+
+        /// <summary>
+        /// Provides a deterministic way to delete the MainPageViewModel property.
+        /// </summary>
+        public static void ClearMainPageViewModel()
+        {
+            _mainPageViewModel.Cleanup();
+            _mainPageViewModel = null;
+        }
+
+        /// <summary>
+        /// Provides a deterministic way to create the MainPageViewModel property.
+        /// </summary>
+        public static void CreateMainPageViewModel()
+        {
+            if (_mainPageViewModel == null)
+            {
+                _mainPageViewModel = new MainPageViewModel();
+            }
+        }
+
+
+        #endregion
+
+        #region TimeoutSettingsViewModel
+        private static TimeoutSettingsViewModel _timeoutSettingsViewModel;
+
+        /// <summary>
+        /// Gets the TimeoutSettingsViewModel property.
+        /// </summary>
+        public static TimeoutSettingsViewModel TimeoutSettingsViewModelStatic
+        {
+            get
+            {
+                if (_timeoutSettingsViewModel == null)
+                {
+                    CreateTimeoutSettingsViewModel();
+                }
+
+                return _timeoutSettingsViewModel;
+            }
+        }
+
+        /// <summary>
+        /// Gets the TimeoutSettingsViewModel property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public TimeoutSettingsViewModel TimeoutSettingsViewModel
+        {
+            get
+            {
+                return TimeoutSettingsViewModelStatic;
+            }
+        }
+
+        /// <summary>
+        /// Provides a deterministic way to delete the TimeoutSettingsViewModel property.
+        /// </summary>
+        public static void ClearTimeoutSettingsViewModel()
+        {
+            _timeoutSettingsViewModel.Cleanup();
+            _timeoutSettingsViewModel = null;
+        }
+
+        /// <summary>
+        /// Provides a deterministic way to create the TimeoutSettingsViewModel property.
+        /// </summary>
+        public static void CreateTimeoutSettingsViewModel()
+        {
+            if (_timeoutSettingsViewModel == null)
+            {
+                _timeoutSettingsViewModel = new TimeoutSettingsViewModel();
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Cleans up all the resources.
+        /// </summary>
+        public static void Cleanup()
+        {
+            ClearMainPageViewModel();
+            ClearTimeoutSettingsViewModel();
+        }
+    }
+}
--- PhoneApp1/ViewModel/TimeoutSettingsViewModel.cs
+++ PhoneApp1/ViewModel/TimeoutSettingsViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TimerUI.ViewModel
 {
-    class TimeoutSettingsViewModel
+    public class TimeoutSettingsViewModel
     {
+        public string VoiceTimeout { get; set; }
+
+        public TimeoutSettingsViewModel()
+        {
+            //TODO: move to app load
+            if (!IsolatedStorageSettings.ApplicationSettings.Contains("VoiceTimeout"))
+            {
+                IsolatedStorageSettings.ApplicationSettings.Add("VoiceTimeout", TimeSpan.FromMinutes(5));
+            }
+            VoiceTimeout = IsolatedStorageSettings.ApplicationSettings["VoiceTimeout"].ToString();
+        }
+
+        public void Cleanup()
+        {
+        }
     }
 }

860483a3bb5fb8a5076ab2ce30d8db2ddb323e61
Rich Hildebrand <richhildebrand@gmail.com>
2013-08-01 22:46:09 -0400

    added constants in time formatter

--- PhoneApp1/TimeFormatter.cs
+++ PhoneApp1/TimeFormatter.cs
@@ -5,15 +5,20 @@ namespace TimerUI
 {
     public class TimeFormatter
     {
+        private static readonly int ONE_SECOND = 1000;
+        private static readonly int ONE_MINUTE = 60000;
+
         public string FormatMiliseconds(long miliseconds)
         {
+
+
             string displayTime;
             TimeSpan t = TimeSpan.FromMilliseconds(miliseconds);
-            if (miliseconds < 1000)
+            if (miliseconds < ONE_SECOND)
             {
                 displayTime = miliseconds.ToString();
             }
-            else if (miliseconds < 60000)
+            else if (miliseconds < ONE_MINUTE)
             {
                 displayTime = string.Format("{0:D1}:{1:D1}", t.Seconds, t.Milliseconds);
             }

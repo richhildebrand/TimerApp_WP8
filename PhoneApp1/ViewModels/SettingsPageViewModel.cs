0e433107396f357417a9a29ccc233962a8a205b6
Eric Fleming <eric-fleming18@hotmail.com>
2013-08-04 14:56:03 -0400

    You can now navigate back to the main page from the settings page

--- PhoneApp1/ViewModels/SettingsPageViewModel.cs
+++ PhoneApp1/ViewModels/SettingsPageViewModel.cs
@@ -8,9 +8,38 @@ namespace TimerUI.ViewModels
     public class SettingsPageViewModel : PropertyChangedBase
     {
         private int _voiceTimeout;
+        private string _addMenuItemText;
+        private Uri _icon;
+        private readonly INavigationService _navigationService;
 
         public int MinVoiceTimeout { get { return 1; } }
         public int MaxVoiceTimeout { get { return 15; } }
+
+        public SettingsPageViewModel(INavigationService navigationService)
+        {
+            var voiceTimeoutSetting = SettingsManager.Get<TimeSpan>(SettingsManager.Settings.VoiceTimeout);
+            _voiceTimeout = voiceTimeoutSetting.Minutes;
+            _navigationService = navigationService;
+            AddMenuItemText = "Timer";
+            Icon = new Uri("/Images/appbar.settings.png", UriKind.Relative);
+        }
+
+        public string AddMenuItemText
+        {
+            get { return _addMenuItemText; }
+            set { _addMenuItemText = value; NotifyOfPropertyChange(() => AddMenuItemText); }
+        }
+
+        public Uri Icon
+        {
+            get { return _icon; }
+            set { _icon = value; NotifyOfPropertyChange(() => Icon); }
+        }
+
+        public void NavigateToMainPage()
+        {
+            _navigationService.UriFor<MainPageViewModel>().Navigate();
+        }
 
         public int VoiceTimeout { 
             get { return _voiceTimeout; }
@@ -20,12 +49,6 @@ namespace TimerUI.ViewModels
                 SettingsManager.Set<TimeSpan>(SettingsManager.Settings.VoiceTimeout, TimeSpan.FromMinutes(value));
                 NotifyOfPropertyChange(() => VoiceTimeout);
             }
-        }
-
-        public SettingsPageViewModel()
-        {
-            var voiceTimeoutSetting = SettingsManager.Get<TimeSpan>(SettingsManager.Settings.VoiceTimeout);
-            _voiceTimeout = voiceTimeoutSetting.Minutes;
         }
     }
 }

bc85e38039038aab260e621522b6fe58dd719d61
Rich Hildebrand <richhildebrand@gmail.com>
2013-08-10 22:07:22 -0400

    Moved logic to viewModel

--- PhoneApp1/ViewModels/EditSettingPageViewModel.cs
+++ PhoneApp1/ViewModels/EditSettingPageViewModel.cs
@@ -1,10 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Controls;
+using Caliburn.Micro;
+using Telerik.Windows.Controls;
+using TimerUI.AppInit;
 
 namespace TimerUI.ViewModels
 {
-    class EditSettingPageViewModel
+    public class EditSettingPageViewModel : PropertyChangedBase
     {
+        private List<String> _voiceCommandList;
 
+        public List<String> VoiceCommandList {
+            get { return _voiceCommandList; }
+            set { _voiceCommandList = value; NotifyOfPropertyChange(() => VoiceCommandList); }
+        }
+
+        public EditSettingPageViewModel()
+        {
+            UpdateVoiceCommandList();
+        }
+
+        public void AddNewStopCommand()
+        {
+            TextBlock messageTextBlock = new TextBlock();
+            RadInputPrompt.Show("Add a new stop command",
+                                MessageBoxButtons.OKCancel,
+                                messageTextBlock,
+                                closedHandler: (arg) => OnStopInputClose(arg)
+            );
+        }
+
+        private void UpdateVoiceCommandList()
+        {
+            var voiceCommands = SettingsManager.Get<List<RecognizableString>>(SettingsManager.Settings.StopVoiceCommands);
+            var stringListOfVoiceCommands = new List<string>();
+            voiceCommands.ForEach(vc => stringListOfVoiceCommands.Add(vc.Value));
+            VoiceCommandList = stringListOfVoiceCommands;
+        }
+
+        public void OnStopInputClose(InputPromptClosedEventArgs arg)
+        {
+            if (arg.Result == DialogResult.OK)
+            {
+                if (IsValidCommand(arg.Text))
+                {
+                    SettingsManager.AddNewVoiceCommand(SettingsManager.Settings.StopVoiceCommands, arg.Text.Trim());
+                    UpdateVoiceCommandList();
+                }
+            }
+        }
+
+        private bool IsValidCommand(string newCommand)
+        {
+            return (newCommand != null) && (newCommand.Trim().Length > 0);
+        }
     }
 }
--- PhoneApp1/Views/EditSettingPage.xaml.cs
+++ PhoneApp1/Views/EditSettingPage.xaml.cs
@@ -8,47 +8,11 @@ using TimerUI.AppInit;
 
 namespace TimerUI.Views
 {
-    public partial class EditSettingPage : PhoneApplicationPage
+    public partial class EditSettingPage
     {
         public EditSettingPage()
         {
             InitializeComponent();
-            UpdateVoiceCommandList();
-        }
-
-        private void UpdateVoiceCommandList()
-        {
-            var voiceCommands = SettingsManager.Get<List<RecognizableString>>(SettingsManager.Settings.StopVoiceCommands);
-            var stringListOfVoiceCommands = new List<string>();
-            voiceCommands.ForEach(vc => stringListOfVoiceCommands.Add(vc.Value));
-            VoiceCommandList.ItemsSource = stringListOfVoiceCommands;
-        }
-
-        private void AddNewStopCommand(object sender, System.Windows.Input.GestureEventArgs e)
-        {
-            TextBlock messageTextBlock = new TextBlock();
-            RadInputPrompt.Show("Add a new stop command", 
-                                MessageBoxButtons.OKCancel, 
-                                messageTextBlock,
-                                closedHandler: (arg) => OnStopInputClose(arg)
-            );
-        }
-
-        public void OnStopInputClose(InputPromptClosedEventArgs arg)
-        {
-            if (arg.Result == DialogResult.OK)
-            {
-                if (IsValidCommand(arg.Text))
-                {
-                    SettingsManager.AddNewVoiceCommand(SettingsManager.Settings.StopVoiceCommands, arg.Text.Trim());
-                    UpdateVoiceCommandList();
-                }
-            }
-        }
-
-        private bool IsValidCommand(string newCommand)
-        {
-            return (newCommand != null) && (newCommand.Trim().Length > 0);
         }
     }
 }
